0efbf8bf0fae89750927ca441961a04bbc466d48
Ruan <xslackx@live.fr>
2024-10-23 16:34:54 -0300

    update parse_feed

--- consume_feed.py
+++ consume_feed.py
@@ -1,7 +1,9 @@
 import urllib.request
 import xml.etree.ElementTree as ET
+import re
 from html.parser import HTMLParser
 from abc import ABC, abstractmethod
+from html import unescape
 from copy import deepcopy
 try: import gc; gc.enable()
 except: pass
@@ -27,7 +29,7 @@ class HackDay(FeedNews):
             "pub_date": str,
             "raw_html": str,
             "description": str,
-            "content": str,
+            "content": [],
             "link": str,
             "creator": str,
             "category": []
@@ -51,21 +53,50 @@ class HackDay(FeedNews):
     def parse_feed(self) -> None:
         root = ET.parse("./feeds/feed.xml")
         items = []
+        description_re = r"<\/div>(.*)<a"
+        content_re = r"<p>(.*)<\/p>"
+        
         for child in root.iter():
             if child.tag == "item":
                 items.append(child)
+        
+        def iter_elements(html, tag):
+            matches = ""
+            if tag == "description":
+                matches = re.finditer(description_re, html, re.MULTILINE)
+            
+            if tag == "content":
+                matches = re.finditer(content_re, html, re.MULTILINE)
+                stanzas = []
+            
+            if tag == "" or tag == None:
+                return
                 
+            for matchNum, match in enumerate(matches, start=1):
+                for groupNum in range(0, len(match.groups())):
+                    groupNum = groupNum + 1
+                    
+                    if tag == "description":
+                        return unescape(match.group(groupNum))
+                    
+                    if tag == "content":
+                       stanzas.append(unescape(match.group(groupNum)))
+            
+            if len(stanzas) > 0 and tag == "content":
+                return stanzas
+            
         def explode(data):
             let = deepcopy(self.schema)
             for element in data:
                 if element.tag == 'title': let["title"] = element.text
                 if element.tag == 'link': let["link"] = element.text
-                if element.tag == 'description': let["description"] = element.text
+                if element.tag == 'description': let["description"] = iter_elements(element.text, "description")
                 if element.tag == 'pubDate': let["pub_date"] = element.text
                 if element.tag == 'category': let['category'].append(element.text)  
                 if element.tag == '{http://purl.org/dc/elements/1.1/}creator':
                     let["creator"] = element.text
                 if element.tag == "{http://purl.org/rss/1.0/modules/content/}encoded":
-                    let["content"] = element.text
+                    let["content"] = iter_elements(element.text, "content")
                     return let
+        
         self.articles = list(map(explode, items))
